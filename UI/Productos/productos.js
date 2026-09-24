01bcd3a05ba482e7ffb7277a3a9fc64348b78f96
Sofia Arevalo <yoalisofia.arevalo@gmail.com>
2026-09-24 15:24:12 -0600

    Se añadio archivo productos.js

---
+++ UI/Productos/productos.js
@@ -0,0 +1,14 @@
+// ================================================================
+// 1. INFORMACIÓN DE LOS PRODUCTOS
+// Este arreglo funciona como una pequeña base de datos local.
+// ================================================================
+
+
+
+
+
+
+// ================================================================
+// 2. REFERENCIAS A ELEMENTOS DEL DOCUMENTO
+// querySelector permite encontrar elementos usando selectores de CSS.
+// ================================================================
