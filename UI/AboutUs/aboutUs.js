1a74d3d5a2a06c619d3006e011eabfca5e5a26d7
Aslan Lopez <aslanlopezmx@gmail.com>
2026-09-23 22:52:52 -0600

    se pule el diseño de sobre nosotros

--- UI/AboutUs/aboutUs.js
+++ UI/AboutUs/aboutUs.js
@@ -1,11 +1,11 @@
 //todo =============== COPIA DE CÓDIGO DE NAVBAR GENERAL===============
-
-
-
+ 
+ 
+ 
 // ================================================================
 // LÓGICA PARA LA PÁGINA SOBRE NOSOTROS (ABOUT US)
 // ================================================================
-
+ 
 document.addEventListener("DOMContentLoaded", () => {
   // Inicialización o scripts específicos para la vista Sobre Nosotros si se requieren.
   console.log("Página Sobre Nosotros cargada correctamente.");
